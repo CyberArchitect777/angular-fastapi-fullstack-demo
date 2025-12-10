0682af1214db032ecc06c96709c06e5603cbb0c0
CyberArchitect777 <169656116+CyberArchitect777@users.noreply.github.com>
2025-12-10 19:15:40 +0000

    continue work on integrating angular and fastapi experimentally

--- fastapi/main.py
+++ fastapi/main.py
@@ -11,24 +11,29 @@ storage_path = os.path.join("storage")
 app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:4200"], allow_methods=["*"], allow_headers=["*"])
 
 @app.post("/process")
-async def upload_file(output_format, file):
+async def upload_file(request):
+    print(request)
+    form_data = await request.form()
+    file_output_format = form_data.post("output-format")
+    file = form_data.post("file")
     file_location = os.path.join(storage_path, "input_" + file.filename)
     file_extension = file.filename.split(".")[-1]
+    print(file.filename)
     # Read file and save to storage
     with open(file_location, "wb+") as file_object:
         file_object.write(file.file.read())
     supported_input_extensions = ["csv", "xlsx", "parquet"]
-    if file_extension in supported_input_extensions and output_format in supported_input_extensions:
-        if file_extension == "csv":
+    if file_extension.lower() in supported_input_extensions and file_output_format in supported_input_extensions:
+        if file_extension.lower() == "csv":
             df = pd.read_csv(file_location)
-        elif file_extension in ["xls", "xlsx"]:
+        elif file_extension.lower() == "xlsx":
             df = pd.read_excel(file_location)
         else:
             df = pd.read_parquet(file_location)
-    output_file_location = os.path.join(storage_path, "output." + output_format)
-    if output_format == "csv":
+    output_file_location = os.path.join(storage_path, "output." + file_output_format)
+    if file_output_format == "csv":
         df.to_csv(output_file_location, index=False)
-    elif output_format in ["xlsx"]:
+    elif file_output_format in ["xlsx"]:
         df.to_excel(output_file_location, index=False)
     else:
         df.to_parquet(output_file_location, index=False)
